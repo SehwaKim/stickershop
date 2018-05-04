0d4593b5202ff787729c01c340b0281176a1908f
김세화 <sehwakm@gmail.com>
2018-05-04 22:19:11 +0900

    정적 resource 서비스 되도록 addResourceHandlers 메서드에 정적자원 경로등록

--- src/main/java/com/boot/stickershop/StickershopApplication.java
+++ src/main/java/com/boot/stickershop/StickershopApplication.java
@@ -2,11 +2,23 @@ package com.boot.stickershop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class StickershopApplication {
+public class StickershopApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StickershopApplication.class, args);
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        if(!registry.hasMappingForPattern("/static/**")){
+            registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        }
+		/*if(!registry.hasMappingForPattern("/webjars/**")){
+            registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:META-INF/resources/webjars/**");
+        }*/
 	}
 }
