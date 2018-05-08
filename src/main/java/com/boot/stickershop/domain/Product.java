b82131b1e99321a3e8f4b02bca9273dd17dcccd0
김세화 <sehwakm@gmail.com>
2018-05-08 10:09:04 +0900

    update: sales (매출량) 필드 추가

--- src/main/java/com/boot/stickershop/domain/Product.java
+++ src/main/java/com/boot/stickershop/domain/Product.java
@@ -21,6 +21,7 @@ public class Product {
     private String name;
     private String price;
     private String size;
+    private Long sales;
     private String imagePath;
 
     @ManyToOne(targetEntity = ProductCategory.class,fetch=FetchType.EAGER)
