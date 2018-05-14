9cb7599d01853b3d348d7edacf72cf9fae3784eb
김세화 <sehwakm@gmail.com>
2018-05-14 16:09:10 +0900

    feat: 주문 상태, 결제 방법 코드로 관리하기 위한 final 클래스 생성

---
+++ src/main/java/com/boot/stickershop/util/OrderCode.java
@@ -0,0 +1,12 @@
+package com.boot.stickershop.util;
+
+public final class OrderCode {
+    public static final int PAYMENT_CARD = 1;
+    public static final int PAYMENT_CASH = 2;
+
+    public static final int STATUS_ORDERED = 3;
+    public static final int STATUS_ONSHPPING = 4;
+    public static final int STATUS_DELIVERED = 5;
+    public static final int STATUS_CANCEL = 6;
+    public static final int STATUS_REFUND = 7;
+}
