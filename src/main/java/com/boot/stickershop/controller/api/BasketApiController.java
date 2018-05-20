aa7140a565cc19b3026b1eb1cf24b3180ce39824
김세화 <sehwakm@gmail.com>
2018-05-20 22:05:37 +0900

    update: 장바구니 삭제 비회원도 가능하게 수정

--- src/main/java/com/boot/stickershop/controller/api/BasketApiController.java
+++ src/main/java/com/boot/stickershop/controller/api/BasketApiController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
 import java.security.Principal;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 @RestController
@@ -22,37 +24,36 @@ public class BasketApiController {
     @Autowired
     UserService userService;
 
-    @DeleteMapping("/deleteAll")
+    @DeleteMapping
     public String deleteAll(Principal principal, HttpSession session){
         if(principal != null){
             User user = userService.getUserByEmail(principal.getName());
             basketService.deleteAllByUserId(user.getId());
         }else {
-            Map<Long, Integer> basket = (Map) session.getAttribute("basket");
-            if(basket != null){
-                for(Long productId : basket.keySet()){
-                    basket.remove(productId);
-                }
-            }
+            Map<Long, Integer> basket = new HashMap<>();
+            session.setAttribute("basket", basket);
         }
         return "ok";
     }
 
-    @PostMapping("/delete")
-    public String delete(@RequestParam Long productId, Principal principal, HttpSession session){
+    @DeleteMapping(value = "/{productId}")
+    public int delete(@PathVariable(value = "productId") Long productId, Principal principal, HttpSession session){
+        int basketSize = 0;
         if(principal != null){
             User user = userService.getUserByEmail(principal.getName());
             basketService.deleteByBasketId(productId);
+            basketSize = basketService.getBasket(user.getId()).size();
         }else {
             Map<Long, Integer> basket = (Map) session.getAttribute("basket");
             if(basket != null && basket.size() > 0){
                 if(basket.containsKey(productId)){
                     basket.remove(productId);
                     session.setAttribute("basket", basket);
+                    basketSize = basket.size();
                 }
             }
         }
-        return "ok";
+        return basketSize;
     }
 
     @PutMapping("/update")
