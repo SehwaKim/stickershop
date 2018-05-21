d89a20e8c82886c8cdf46a60ae05e33ab7edd92a
오세진 <jinioh88@gmail.com>
2018-05-21 15:14:25 +0900

    image 경로 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,19 +4,19 @@ insert into product_categories(id, name) values (3, 'linux');
 insert into product_categories(id, name) values (4, 'tool');
 insert into product_categories(id, name) values (5, 'free');
 
-insert into products(id, name, price, size, sales, image_path, category_id, regtime, edittime) VALUES (1, 'Spring', 1500, '40 x 40 mm', 1, '/static/spring.png', 2, now(), now());
-insert into products(id, name, price, size, sales, image_path, category_id, regtime, edittime) VALUES (2, 'Python', 1200, '40 x 40 mm', 90, '/static/python.png', 2, now(), now());
-insert into products(id, name, price, size, sales, image_path, category_id, regtime, edittime) VALUES (3, 'Django', 0, '40 x 40 mm', 8, '/static/django.png', 2, now(), now());
-insert into products(id, name, price, size, sales, image_path, category_id, regtime, edittime) VALUES (4, 'Groovy', 0, '40 x 40 mm', 70, '/static/groovy.png', 2, now(), now());
-insert into products(id, name, price, size, sales, image_path, category_id, regtime, edittime) VALUES (5, 'Docker', 2000, '40 x 40 mm', 9, '/static/docker.png', 4, now(), now());
-insert into products(id, name, price, size, sales, image_path, category_id, regtime, edittime) VALUES (6, 'IntelliJ', 1300, '40 x 40 mm', 80, '/static/intellij.png', 4, now(), now());
-insert into products(id, name, price, size, sales, image_path, category_id, regtime, edittime) VALUES (7, 'Android', 10000, '40 x 40 mm', 7, '/static/android.png', 2, now(), now());
-insert into products(id, name, price, size, sales, image_path, category_id, regtime, edittime) VALUES (8, 'Tensorflow', 20000, '40 x 40 mm', 2, '/static/tensorflow.png', 2, now(), now());
-insert into products(id, name, price, size, sales, image_path, category_id, regtime, edittime) VALUES (9, 'Git', 30000, '40 x 40 mm', 6, '/static/git.png', 4, now(), now());
-insert into products(id, name, price, size, sales, image_path, category_id, regtime, edittime) VALUES (10, 'Rails', 3000, '40 x 40 mm', 5, '/static/radis.png', 2, now(), now());
+insert into products(id, name, price, size, sales, image_path, category_id, regtime, edittime) VALUES (1, 'Spring', 1500, '40 x 40 mm', 1, '/images/spring.png', 2, now(), now());
+insert into products(id, name, price, size, sales, image_path, category_id, regtime, edittime) VALUES (2, 'Python', 1200, '40 x 40 mm', 90, '/images/python.png', 2, now(), now());
+insert into products(id, name, price, size, sales, image_path, category_id, regtime, edittime) VALUES (3, 'Django', 0, '40 x 40 mm', 8, '/images/django.png', 2, now(), now());
+insert into products(id, name, price, size, sales, image_path, category_id, regtime, edittime) VALUES (4, 'Groovy', 0, '40 x 40 mm', 70, '/images/groovy.png', 2, now(), now());
+insert into products(id, name, price, size, sales, image_path, category_id, regtime, edittime) VALUES (5, 'Docker', 2000, '40 x 40 mm', 9, '/images/docker.png', 4, now(), now());
+insert into products(id, name, price, size, sales, image_path, category_id, regtime, edittime) VALUES (6, 'IntelliJ', 1300, '40 x 40 mm', 80, '/images/intellij.png', 4, now(), now());
+insert into products(id, name, price, size, sales, image_path, category_id, regtime, edittime) VALUES (7, 'Android', 10000, '40 x 40 mm', 7, '/images/android.png', 2, now(), now());
+insert into products(id, name, price, size, sales, image_path, category_id, regtime, edittime) VALUES (8, 'Tensorflow', 20000, '40 x 40 mm', 2, '/images/tensorflow.png', 2, now(), now());
+insert into products(id, name, price, size, sales, image_path, category_id, regtime, edittime) VALUES (9, 'Git', 30000, '40 x 40 mm', 6, '/images/git.png', 4, now(), now());
+insert into products(id, name, price, size, sales, image_path, category_id, regtime, edittime) VALUES (10, 'Rails', 3000, '40 x 40 mm', 5, '/images/radis.png', 2, now(), now());
 
-insert into product_files(id, file_name, save_file_name, content_type, length, regtime, product_id) VALUES (1, 'python2.jpg', '/static/python2.jpg', 'image/jpg', 19600, now(), 2);
-insert into product_files(id, file_name, save_file_name, content_type, length, regtime, product_id) VALUES (2, 'python3.jpg', '/static/python3.jpg', 'image/jpg', 19600, now(), 2);
+insert into product_files(id, file_name, save_file_name, content_type, length, regtime, product_id) VALUES (1, 'python2.jpg', '/images/python2.jpg', 'image/jpg', 19600, now(), 2);
+insert into product_files(id, file_name, save_file_name, content_type, length, regtime, product_id) VALUES (2, 'python3.jpg', '/images/python3.jpg', 'image/jpg', 19600, now(), 2);
 
 insert into users(id, name, email, password, regtime) VALUES (1, '테스트', 'test', '{bcrypt}$2a$10$qS48/8nM2fSagy1di.whF.tutE/VZ9/wwOkGBcm.Ty8mOKLfwpv/G', now());
 insert into user_roles(id, role_name, user_id) VALUES (1, 'USER', 1);
